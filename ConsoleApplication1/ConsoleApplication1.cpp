4a94787c1bb559e1f99c552d94e6bc033f233312
hamstler <Knaskreski@gmail.com>
2022-05-04 17:29:01 +0200

    Extend metadata information

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -30,9 +30,22 @@ void fill() {
 int main() {
     fill();
 
-    cout << "Bits per pixel:" << info_header->biBitCount << endl;
+    cout << "Type of file: " << file_header->bfType << endl;
     cout << "Size of file in bytes: " << file_header->bfSize << endl;
+    cout << "Reserved1: " << file_header->bfReserved1 << endl;
+    cout << "Reserved2: " << file_header->bfReserved2 << endl;
+    cout << "OffBits: " << file_header->bfOffBits << endl;
+
+    cout << "Size of heading:" << info_header->biSize << endl;
     cout << "Resolution of file (Width/Height): " << info_header->biWidth << "px " << " " << info_header->biHeight << "px " << endl;
+    cout << "Color planes:" << info_header->biPlanes << endl;
+    cout << "Bits per pixel:" << info_header->biBitCount << endl;
+    cout << "Compression:" << info_header->biCompression << endl;
+    cout << "Size of image:" << info_header->biSizeImage << endl;
+    cout << "Vertical resolution:" << info_header->biXPelsPerMeter << endl;
+    cout << "Horizontal resolution :" << info_header->biYPelsPerMeter << endl;
+    cout << "Color palette count:" << info_header->biClrUsed << endl;
+    cout << "Important colors in palette:" << info_header->biClrImportant << endl;
 
     return 0;
 }
